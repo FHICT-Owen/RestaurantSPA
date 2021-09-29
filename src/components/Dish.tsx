d3128a0b308637be6d362eeaa91bd099f7595fd3
DirkLemmen <33750270+DirkLemmen@users.noreply.github.com>
2021-09-29 12:11:23 +0200

    added styling to dish component

--- src/components/Dish.tsx
+++ src/components/Dish.tsx
@@ -1,17 +1,30 @@
+import "./Dish.css";
+
+
+
+// const Footer = () => <div style={footerStyle}>Discover unlimited possibilities</div>;
+
 export default function Dish(props: any) {
+  var imageStyle = {
+  background: `url("data:image/jpeg;base64,${btoa(String.fromCharCode(...new Uint8Array(props.image)))}")`,
+  };
+
 	return (
 		<li>
-      <div className="container menu-card">
-        <div className="menu-item">
-          <div>
-            <img className="menu-image" src={ "data:image/jpeg;base64," + btoa(String.fromCharCode(...new Uint8Array(props.image))) } alt="menu-item"/>
-          </div>
-          <div className="menu-text">
-            <h5>{props.name}</h5>
-            <p className="menu-description">{props.description}</p>
-          </div>
+      <div className="food-card">
+        <div className="menu-card">
+            <div className="menu-image" style={imageStyle}></div>
+            
+              <div className="menu-title">
+                <h5>{props.name}</h5>
+              </div>
+              <div className="description-frame">
+                <p className="menu-description">{props.description}</p>
+              </div>
+              <div className="plus-icon"></div>
+            </div>
         </div>
-      </div>
     </li>
 	)
 }
+// {"data:image/png;base64," + props.image}
