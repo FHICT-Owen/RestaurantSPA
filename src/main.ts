47426a8a967f6eb4e70bbc2fbb5ff9ff7a657dea
OwendB1 <52082575+OwendB1@users.noreply.github.com>
2021-10-05 13:28:39 +0200

    Edited project to use local environment files

--- src/main.ts
+++ src/main.ts
@@ -16,9 +16,9 @@ async function init() {
                     : window.location.pathname,
             )
         },
-        clientId: process.env.VUE_APP_AUTH0_CLIENT_KEY,
-        domain: process.env.VUE_APP_AUTH0_DOMAIN,
-        audience: process.env.VUE_APP_AUTH0_AUDIENCE,
+        clientId: process.env.local.VUE_APP_AUTH0_CLIENT_KEY,
+        domain: process.env.local.VUE_APP_AUTH0_DOMAIN,
+        audience: process.env.local.VUE_APP_AUTH0_AUDIENCE,
         redirectUri: window.location.origin,
     });
     const app = createApp(App);
