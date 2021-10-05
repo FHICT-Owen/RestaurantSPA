f91f414a967c38732d9e5494a6467623b60658d1
OwendB1 <52082575+OwendB1@users.noreply.github.com>
2021-10-05 15:54:50 +0200

    Added Vue config + corrected syntax

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,13 +4,13 @@
       <div class="container">
         <div class="navbar-brand logo"></div>
         <button
-                class="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarNav"
-                aria-controls="navbarNav"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -21,7 +21,9 @@
               <router-link to="/" class="nav-link">Home</router-link>
             </li>
             <li class="nav-item" v-if="isAuthenticated">
-              <router-link to="/external-api" class="nav-link">External API</router-link>
+              <router-link to="/external-api" class="nav-link"
+                >External API</router-link
+              >
             </li>
             <li class="nav-item" v-if="isAuthenticated">
               <router-link to="/menu" class="nav-link">Menu</router-link>
@@ -30,35 +32,44 @@
           <ul class="navbar-nav d-none d-md-block">
             <li v-if="!isAuthenticated && !loading" class="nav-item">
               <button
-                      id="qsLoginBtn"
-                      class="btn btn-primary btn-margin"
-                      @click.prevent="login"
-              >Login
+                id="qsLoginBtn"
+                class="btn btn-primary btn-margin"
+                @click.prevent="login"
+              >
+                Login
               </button>
             </li>
 
             <li class="nav-item dropdown" v-if="isAuthenticated">
               <a
-                      class="nav-link dropdown-toggle"
-                      href="#"
-                      id="profileDropDown"
-                      data-toggle="dropdown"
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="profileDropDown"
+                data-toggle="dropdown"
               >
                 <img
-                        :src="user.picture"
-                        alt="User's profile picture"
-                        class="nav-user-profile rounded-circle"
-                        width="50"
+                  :src="user.picture"
+                  alt="User's profile picture"
+                  class="nav-user-profile rounded-circle"
+                  width="50"
                 />
               </a>
               <div class="dropdown-menu dropdown-menu-right">
                 <div class="dropdown-header">{{ user.name }}</div>
-                <router-link to="/profile" class="dropdown-item dropdown-profile">
-                  <font-awesome-icon class="mr-3" icon="user"/>
+                <router-link
+                  to="/profile"
+                  class="dropdown-item dropdown-profile"
+                >
+                  <font-awesome-icon class="mr-3" icon="user" />
                   Profile
                 </router-link>
-                <a id="qsLogoutBtn" href="#" class="dropdown-item" @click.prevent="logout">
-                  <font-awesome-icon class="mr-3" icon="power-off"/>
+                <a
+                  id="qsLogoutBtn"
+                  href="#"
+                  class="dropdown-item"
+                  @click.prevent="logout"
+                >
+                  <font-awesome-icon class="mr-3" icon="power-off" />
                   Log out
                 </a>
               </div>
@@ -66,33 +77,41 @@
           </ul>
 
           <ul class="navbar-nav d-md-none" v-if="!isAuthenticated && !loading">
-            <button id="qsLoginBtn" class="btn btn-primary btn-block" @click="login">Log in</button>
+            <button
+              id="qsLoginBtn"
+              class="btn btn-primary btn-block"
+              @click="login"
+            >
+              Log in
+            </button>
           </ul>
 
           <ul
-                  id="mobileAuthNavBar"
-                  class="navbar-nav d-md-none d-flex"
-                  v-if="isAuthenticated"
+            id="mobileAuthNavBar"
+            class="navbar-nav d-md-none d-flex"
+            v-if="isAuthenticated"
           >
             <li class="nav-item">
               <span class="user-info">
                 <img
-                        :src="user.picture"
-                        alt="User's profile picture"
-                        class="nav-user-profile d-inline-block rounded-circle mr-3"
-                        width="50"
+                  :src="user.picture"
+                  alt="User's profile picture"
+                  class="nav-user-profile d-inline-block rounded-circle mr-3"
+                  width="50"
                 />
                 <h6 class="d-inline-block">{{ user.name }}</h6>
               </span>
             </li>
             <li>
-              <font-awesome-icon icon="user" class="mr-3"/>
+              <font-awesome-icon icon="user" class="mr-3" />
               <router-link to="/profile">Profile</router-link>
             </li>
 
             <li>
-              <font-awesome-icon icon="power-off" class="mr-3"/>
-              <a id="qsLogoutBtn" href="#" class @click.prevent="logout">Log out</a>
+              <font-awesome-icon icon="power-off" class="mr-3" />
+              <a id="qsLogoutBtn" href="#" class @click.prevent="logout"
+                >Log out</a
+              >
             </li>
           </ul>
         </div>
@@ -102,32 +121,32 @@
 </template>
 
 <script>
-import { inject } from 'vue';
+import { inject } from "vue";
 
 export default {
-    name:    "NavBar",
-    inject:  ['Auth'],
-    methods: {
-        login() {
-            this.Auth.loginWithRedirect();
-        },
-        logout() {
-            this.Auth.logout();
-            this.$router.push({path: "/"});
-        }
+  name: "NavBar",
+  inject: ["Auth"],
+  methods: {
+    login() {
+      this.Auth.loginWithRedirect();
     },
-    setup() {
-        const auth = inject("Auth");
-        return {
-            ...auth
-        }
-    }
+    logout() {
+      this.Auth.logout();
+      this.$router.push({ path: "/" });
+    },
+  },
+  setup() {
+    const auth = inject("Auth");
+    return {
+      ...auth,
+    };
+  },
 };
 </script>
 
 <style>
 #mobileAuthNavBar {
-    min-height: 125px;
-    justify-content: space-between;
+  min-height: 125px;
+  justify-content: space-between;
 }
 </style>
