49d0ddf035e2bb45cc2af83078de4e843b93fa9d
OwendB1 <52082575+OwendB1@users.noreply.github.com>
2021-10-05 15:21:55 +0200

    Added button hide + Audit fixed a lot of npm packages

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,24 +3,33 @@
     <img class="mb-3 app-logo" src="/logo.png" alt="Vue.js logo" width="120" />
     <h1 class="mb-4">Vue.js Sample Project</h1>
     <p class="lead">
-      This is a sample application that demonstrates an authentication flow for an SPA, using
-      <a
-        href="https://vuejs.org"
-      >Vue.js</a>
+      This is a sample application that demonstrates an authentication flow for
+      an SPA, using
+      <a href="https://vuejs.org">Vue.js</a>
     </p>
-    <ButtonOption />
-    <ButtonComp />
+    <p v-if="isAuthenticated && !loading">
+      <ButtonOption />
+      <ButtonComp />
+    </p>
   </div>
 </template>
 
-<script lang="ts">
-import ButtonOption from './ButtonOptionApi.vue'
-import ButtonComp from './ButtonCompositionApi.vue'
+<script>
+import ButtonOption from "./ButtonOptionApi.vue";
+import ButtonComp from "./ButtonCompositionApi.vue";
+import { inject } from "vue";
 
-export default ({
+export default {
+  inject: ["Auth"],
   components: {
     ButtonOption,
-    ButtonComp
-  }
-})
+    ButtonComp,
+  },
+  setup() {
+    const auth = inject("Auth");
+    return {
+      ...auth
+    };
+  },
+};
 </script>
