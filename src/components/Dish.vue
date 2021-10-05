131a0038f41bacec8d53247d064e1481911f19d1
DirkLemmen <33750270+DirkLemmen@users.noreply.github.com>
2021-10-05 16:54:43 +0200

    dish component

---
+++ src/components/Dish.vue
@@ -0,0 +1,31 @@
+<template>
+    <div className="food-card">
+        <div className="menu-card">
+            <div className="menu-image" style="backgroundImage: {{image}}"></div>
+            <div className="menu-title">
+                <h5>{{dish.name}}</h5>
+            </div>
+            <div className="description-frame">
+                <p className="menu-description">{{dish.description}}</p>
+            </div>
+            <div className="plus-icon"></div>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+    import { onMounted, ref } from 'vue'
+    import {convertNumberArrayToImageUrl} from '../utils'
+
+export default ({
+
+    setup() {
+        const dish = ref<Dish>({id:0,name:"",description:"",image:[]})
+        let image = "";
+
+        onMounted(() => {
+            image = convertNumberArrayToImageUrl(dish.value.image)
+        })
+        return {dish, image};
+    },
+})
+</script>
--- src/views/Menu.vue
+++ src/views/Menu.vue
@@ -3,8 +3,10 @@
     <div class="mb-5">
       <h1>Menu</h1>
       <ul>
-        <li>{{categories}}</li>
-        <li>{{dishes}}</li>
+        <li v-for="category of categories" :key="category.id">{{categories}}</li>
+      </ul>
+      <ul>
+        <Dish :dish="dish" v-for="dish of dishes" :key="dish.id" />
       </ul>
     </div>    
   </div>
@@ -13,14 +15,18 @@
 <script>
 import store from '@/store'
 import { computed } from 'vue'
+import Dish from '../components/Dish.vue'
 
 export default {
     setup() {
       
     let categories = computed(() => store.state.categories)
 
-    let dishes = computed(() => store.state.dishes)
+    let dishes = computed(() => store.state.dishes) 
     return { categories, dishes }
-	}
+	},
+  components:{
+    Dish
+  }
 };
 </script>
