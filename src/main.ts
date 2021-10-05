c0eb7b4669cf8198acd2dacdda4e2fafbb841cc9
OwendB1 <52082575+OwendB1@users.noreply.github.com>
2021-10-05 18:44:42 +0200

    Switched to FireBase based environment config for production

--- src/main.ts
+++ src/main.ts
@@ -8,28 +8,54 @@ import { faLink, faUser, faPowerOff } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import store from "./store";
 
+const functions = require('firebase-functions');
+
 async function init() {
+  if(process.env == null) {
     const AuthPlugin = await Auth0.init({
-        onRedirectCallback: (appState) => {
-            router.push(
-                appState && appState.targetUrl
-                    ? appState.targetUrl
-                    : window.location.pathname,
-            )
-        },
-        clientId: process.env.VUE_APP_AUTH0_CLIENT_KEY,
-        domain: process.env.VUE_APP_AUTH0_DOMAIN,
-        audience: process.env.VUE_APP_AUTH0_AUDIENCE,
-        redirectUri: window.location.origin,
+      onRedirectCallback: (appState) => {
+        router.push(
+          appState && appState.targetUrl
+            ? appState.targetUrl
+            : window.location.pathname,
+        )
+      },
+      clientId: functions.config().auth0.clientid,
+      domain: functions.config().auth0.domain,
+      audience: functions.config().auth0.audience,
+      redirectUri: window.location.origin,
     });
     const app = createApp(App);
     library.add(faLink, faUser, faPowerOff);
     app
-        .use(AuthPlugin)
-        .use(store)
-        .use(router)
-        .component("font-awesome-icon", FontAwesomeIcon)
-        .mount('#app');
+      .use(AuthPlugin)
+      .use(store)
+      .use(router)
+      .component("font-awesome-icon", FontAwesomeIcon)
+      .mount('#app');
+  } else {
+    const AuthPlugin = await Auth0.init({
+      onRedirectCallback: (appState) => {
+        router.push(
+          appState && appState.targetUrl
+            ? appState.targetUrl
+            : window.location.pathname,
+        )
+      },
+      clientId: process.env.VUE_APP_AUTH0_CLIENT_KEY,
+      domain: process.env.VUE_APP_AUTH0_DOMAIN,
+      audience: process.env.VUE_APP_AUTH0_AUDIENCE,
+      redirectUri: window.location.origin,
+    });
+    const app = createApp(App);
+    library.add(faLink, faUser, faPowerOff);
+    app
+      .use(AuthPlugin)
+      .use(store)
+      .use(router)
+      .component("font-awesome-icon", FontAwesomeIcon)
+      .mount('#app');
+  }    
 }
 
 init();
