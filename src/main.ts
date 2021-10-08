f270a0b4bc06a90a59ac2ead65fc27e43cf1678f
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2021-10-08 10:10:57 +0200

    added pwa, move and changed eslint config into package.json + minor changes

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faLink, faUser, faPowerOff } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import store from './store'
+import './registerServiceWorker'
 
 async function init () {
   const AuthPlugin = await Auth0.init({
@@ -17,9 +18,9 @@ async function init () {
           : window.location.pathname
       )
     },
-    clientId: process.env.VUE_APP_AUTH0_CLIENT_KEY,
-    domain: process.env.VUE_APP_AUTH0_DOMAIN,
-    audience: process.env.VUE_APP_AUTH0_AUDIENCE,
+    clientId: process.env.VUE_APP_AUTH0_CLIENT_KEY || '',
+    domain: process.env.VUE_APP_AUTH0_DOMAIN || '',
+    audience: process.env.VUE_APP_AUTH0_AUDIENCE || '',
     redirectUri: window.location.origin
   })
   const app = createApp(App)
