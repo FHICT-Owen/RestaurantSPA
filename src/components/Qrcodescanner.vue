659d6f01a6b1dda03a36ef3bd345895506603bcf
SiemLuc <48807736+SiemLuc@users.noreply.github.com>
2021-10-12 16:00:52 +0200

    Added QR code scanner (Landing page)

---
+++ src/components/Qrcodescanner.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="flex h-screen">
+        <div class="w-1/2 h-96 m-auto">
+            <qrcode-stream @decode='decode' :track='drawOutline'></qrcode-stream>
+        </div>
+    </div>
+</template>
+
+<script>
+import {QrcodeStream} from 'vue3-qrcode-reader'
+
+export default {
+  components: {   
+    QrcodeStream
+  },
+  methods: {
+    decode(decodeString){
+      console.log(decodeString)
+    },
+    drawOutline(decodeData, context){
+      var points = []
+      for(var k in decodeData){
+        switch (k) {
+        case 'topLeftCorner':
+          points[0] = decodeData[k]
+          break   
+        case 'topRightCorner':
+          points[1] = decodeData[k]
+          break   
+        case 'bottomRightCorner':
+          points[2] = decodeData[k]
+          break   
+        case 'bottomLeftCorner':
+          points[3] = decodeData[k]
+          break            
+        default:
+          break
+        }   
+      }
+      context.lineWidth = 5
+      context.strokeStyle = 'green'
+      context.beginPath()
+      console.log(points[0])
+      context.moveTo(points[0].x, points[0].y)
+
+      for(const {x,y} of points){
+        context.lineTo(x,y)
+      }
+
+      context.lineTo(points[0].x, points[0].y)
+      context.closePath()
+      context.stroke()
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <Menu/>
+  <div>
+    <hero />
+    <hr />
+    <qrcodescanner />
+    <Menu />
+  </div>
 </template>
 
-<script lang="ts">
+<script>
+import Hero from '../components/Hero.vue'
 import Menu from '../components/Menu.vue'
-import { computed, ref } from 'vue'
-import store from '@/store'
-
+import Qrcodescanner from '../components/Qrcodescanner.vue'
 
 export default {
   name: 'Home',
   components: {
+    Hero,
     Menu,
-  },
+    Qrcodescanner
+  }
 }
-
 </script>
