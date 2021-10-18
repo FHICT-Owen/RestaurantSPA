c96f0b705317c9e42c84dcb1e1aa1ddb51397cad
DirkLemmen <33750270+DirkLemmen@users.noreply.github.com>
2021-10-18 10:56:49 +0200

    added sessionpage

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
 import Profile from '../views/Profile.vue'
 import Menu from '../views/Menu.vue'
+import SessionPage from '../views/SessionPage.vue'
 
 import { Auth0 } from '@/auth'
 
@@ -9,16 +10,25 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
+    meta: {requiresNavBar: true},
     component: Home
   },
   {
     path: '/menu',
     name: 'menu',
+    meta: {requiresNavBar: true},
     component: Menu,
+  },
+  {
+    path: '/session_page',
+    name: 'sessionpage',
+    meta: {requiresNavBar: false},
+    component: SessionPage,
   },
   {
     path: '/profile',
     name: 'profile',
+    meta: {requiresNavBar: true},
     component: Profile,
     beforeEnter: Auth0.routeGuard
   }
---
+++ src/views/SessionPage.vue
@@ -0,0 +1,38 @@
+<template>
+ <div class="welcome">
+    <div class="welcome-text">
+      <h1>Welcome</h1>
+      <h3>What would you like to have?</h3>
+    </div>
+  </div>
+<CostumerMenu />
+</template>
+
+
+<script lang="ts">
+
+import CostumerMenu from '../components/CustomerMenu.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    CostumerMenu,
+  },
+}
+</script>
+
+
+<style>
+.welcome {
+  position: relative;
+  color: white;
+  height: 200px;
+  background-color: #FFA825;
+}
+.welcome-text {
+  left: 0;
+  bottom: 0;
+  position: absolute;
+  padding: 20px;
+}
+</style>
