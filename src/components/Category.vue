6fa03446f47e71060cc25fffe010ab8d672acd45
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2021-11-02 10:09:21 +0100

    Update Category.vue

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -4,7 +4,7 @@
       <h3 v-if="!isEditMode" @click="openInput" style="display:inline" class="mr-3">{{category.name}} </h3>
       <input v-model="name" v-else @keyup.enter="editCategory">
       <!-- <button class="btn btn-danger btn-sm rounded" style="display:inline" @click="deleteCategory">X</button> -->
-      <q-btn label="Delete" color="danger" @click="confirm = true" />
+      <!-- <q-btn label="Delete" color="danger" @click="confirm = true" />
 
 
       <q-dialog v-model="confirm" persistent>
@@ -18,7 +18,7 @@
             <q-btn flat label="Delete" color="primary" v-close-popup @click="deleteCategory()" />
           </q-card-actions>
         </q-card>
-      </q-dialog>
+      </q-dialog> -->
 
 
     </div>
@@ -26,69 +26,69 @@
 </template>
 
 <script lang="ts">
-  import ConfirmDialog from '../components/ConfirmDialog.vue'
-  import store from '@/store'
-  import {ref} from 'vue'
-  import {useQuasar} from 'quasar'
+import ConfirmDialog from '../components/ConfirmDialog.vue'
+import store from '@/store'
+import {ref} from 'vue'
+import {useQuasar} from 'quasar'
 
-  export default ({
-    props: {
-      category: {} as Category
-    },
-    setup(props: {
+export default ({
+  props: {
+    category: {} as Category
+  },
+  setup(props: {
       category: Category
     }) {
-      const $q = useQuasar()
+    const $q = useQuasar()
 
-      let isEditMode = ref(false)
-      let name = ref('')
+    let isEditMode = ref(false)
+    let name = ref('')
 
-      function deleteCategory() {
-        store.dispatch('deleteCategory', props.category).then(() => {
-          store.dispatch('getAllCategories')
-        })
-      }
-
-      function openInput() {
-        isEditMode.value = !isEditMode.value
-        name.value = props.category.name
-      }
-
-      function editCategory() {
-        store.dispatch('editCategory', {
-          id: props.category.id,
-          name: name.value
-        }).then(() => {
-          store.dispatch('getAllCategories')
-        })
-        isEditMode.value = !isEditMode.value
-      }
-
-      function confirm() {
-        $q.dialog({
-          title: 'Confirm',
-          message: 'Are you sure you want to delete this category',
-          cancel: true,
-          persistent: true
-        }).onOk(() => {
-          console.log('>>>> OK')
-        }).onOk(() => {
-          // console.log('>>>> second OK catcher')
-        }).onCancel(() => {
-          // console.log('>>>> Cancel')
-        }).onDismiss(() => {
-          // console.log('I am triggered on both OK and Cancel')
-        })
-      }
-      return {
-        deleteCategory,
-        isEditMode,
-        editCategory,
-        openInput,
-        name,
-        confirm: ref(false),
-        ConfirmDialog,
-      }
+    function deleteCategory() {
+      store.dispatch('deleteCategory', props.category).then(() => {
+        store.dispatch('getAllCategories')
+      })
     }
-  })
+
+    function openInput() {
+      isEditMode.value = !isEditMode.value
+      name.value = props.category.name
+    }
+
+    function editCategory() {
+      store.dispatch('editCategory', {
+        id: props.category.id,
+        name: name.value
+      }).then(() => {
+        store.dispatch('getAllCategories')
+      })
+      isEditMode.value = !isEditMode.value
+    }
+
+    function confirm() {
+      $q.dialog({
+        title: 'Confirm',
+        message: 'Are you sure you want to delete this category',
+        cancel: true,
+        persistent: true
+      }).onOk(() => {
+        console.log('>>>> OK')
+      }).onOk(() => {
+        // console.log('>>>> second OK catcher')
+      }).onCancel(() => {
+        // console.log('>>>> Cancel')
+      }).onDismiss(() => {
+        // console.log('I am triggered on both OK and Cancel')
+      })
+    }
+    return {
+      deleteCategory,
+      isEditMode,
+      editCategory,
+      openInput,
+      name,
+      confirm: ref(false),
+      ConfirmDialog,
+    }
+  }
+})
 </script>
