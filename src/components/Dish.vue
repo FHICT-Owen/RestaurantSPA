e18e648dad410248781cf75b830b4017a8a5f323
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2021-11-23 15:33:55 +0100

    added add and remove buttons to dish component

--- src/components/Dish.vue
+++ src/components/Dish.vue
@@ -1,14 +1,19 @@
 <template>
   <div 
-    class="flex flex-row rounded-3xl h-24 mt-2"
-    style="box-shadow: -1px 1px 4px 1px rgba(0, 0, 0, 0.1)">
-    <div 
-      class="rounded-3xl m-1 bg-blend-normal bg-cover bg-no-repeat" 
-      :style="{background: dish.image, minHeight: imgSize, minWidth: imgSize}">
+    class="flex flex-col">
+    <div class="flex flex-row rounded-3xl h-24 mt-6" style="box-shadow: 0px 1px 2px 2px rgba(0, 0, 0, 0.1)">
+      <div 
+        class="rounded-3xl m-1 bg-blend-normal bg-cover bg-no-repeat" 
+        :style="{background: dish.image, minHeight: imgSize, minWidth: imgSize}">
+      </div>
+      <div class="flex-1 m-1">
+        <h5 class="font-medium text-lg">{{dish.name}}</h5>
+        <p class="text-gray-400" style="font-size: 0.85rem; line-height: 1rem">{{dish.description}}</p>
+      </div>
     </div>
-    <div class="flex-1 m-1">
-      <h5 class="font-medium text-lg">{{dish.name}}</h5>
-      <p class="text-gray-400" style="font-size: 0.85rem; line-height: 1rem">{{dish.description}}</p>
+    <div v-if="isInSession" class="flex flex-row justify-around rounded-3xl mt-1.5 h-10">
+      <button class="bg-gray-400 text-white font-medium text-sm w-full rounded-l-3xl p-1 my-1 ml-1 mr-0.5">ADD</button>
+      <button class="bg-red-400 text-white font-medium text-sm w-full  rounded-r-3xl p-1 my-1 ml-0.5 mr-1">REMOVE</button>
     </div>
   </div>
 </template>
