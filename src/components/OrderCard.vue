40ed1cffca870ebac3f8c806d5fda77a75712b95
SiemLuc <48807736+SiemLuc@users.noreply.github.com>
2021-11-24 00:19:25 +0100

    Added OrderDishCard

--- src/components/OrderCard.vue
+++ src/components/OrderCard.vue
@@ -2,51 +2,53 @@
     <div  class="w-64 text-white mb-3" style="max-width: 18rem;">
         <!-- TODO: bg-color depends on order status -->
         <div class="card-header bg-warning relative">
-            <p class="text-xl">18:43</p>
-            <h5 class="text-xl">#34</h5>
+            <p class="text-xl">00:00</p>
+            <h5 class="text-xl">#{{order.id}}</h5>
             <div class="absolute inset-y-3 right-5 flex space-x-2">
-                <svg style="width:25px" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20"
-                    fill="currentColor">
-                    <path fillRule="evenodd"
-                        d="M7.707 3.293a1 1 0 010 1.414L5.414 7H11a7 7 0 017 7v2a1 1 0 11-2 0v-2a5 5 0 00-5-5H5.414l2.293 2.293a1 1 0 11-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                        clipRule="evenodd" />
-                </svg>
-                <svg style="width:25px" xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
-                    viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                        d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
-                </svg>
-                <svg style="width:25px" className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd"
-                        d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                        clipRule="evenodd" /></svg>
-                <svg style="width:25px" className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path fillRule="evenodd"
-                        d="M3 6a3 3 0 013-3h10a1 1 0 01.8 1.6L14.25 8l2.55 3.4A1 1 0 0116 13H6a1 1 0 00-1 1v3a1 1 0 11-2 0V6z"
-                        clipRule="evenodd" /></svg>
+                <ReplyIcon class="w-6 h-6" />
+                <BanIcon class="w-6 h-6" />
+                <CheckIcon class="h-6 w-6" />
+                <FlagIcon class="h-6 w-6" />
             </div>
         </div>
         <div class="card-body border bg-light text-black overflow-y-auto max-h-64">
+            <p class="text-m text-gray-700 my-2" >{{order.comments}}</p>
             <table class="table">
-            <tr>
-                <td>2x Carpaccio</td>
-            </tr>
-            <tr>
-                <td>1x Tomato Soup</td>
-            </tr>
-            <tr>
-                <td>1x Bread</td>
-            </tr>
+						<div v-for="(order, index) in order.dishes" :key="index">
+							<tr>
+									<td>{{order}}</td>
+							</tr>
+						</div>
             </table>
         </div>
     </div>
 </template>
 
 
-<script>
-export default {
+<script lang="ts">
+import { computed, defineComponent, PropType} from 'vue'
+import store from '@/store'
+import { FlagIcon, CheckIcon, BanIcon, ReplyIcon } from '@heroicons/vue/solid'
 
-}
+
+export default defineComponent({
+  components: {
+    FlagIcon,
+    CheckIcon,
+    BanIcon,
+    ReplyIcon
+  },
+  props: {
+    order: {
+      type: Object as PropType<Order>,
+      required: true
+    }
+  },
+  setup() {
+    const orders = computed(() => store.state.orders)
+    return {
+      orders 
+    }
+  }
+})
 </script>
--- src/services/OrderDataService.ts
+++ src/services/OrderDataService.ts
@@ -1,6 +1,4 @@
 import store from '@/store'
-import { showPopUp } from '@/utils'
-import { computed } from '@vue/runtime-core'
 import axios from 'axios'
 
 export default class OrderDataService {
@@ -13,39 +11,39 @@ export default class OrderDataService {
     return response.data
   }
 
-//   public static async createOrder(order: Order) {
-//     const token = computed(() => store.state.apiToken)
-//     await axios.post(`${this.API_URL}/dish/`, dish, {
-//       headers: { Authorization: `Bearer ${token.value}`}
-//     })
-//       .then(() => showPopUp(`Added ${dish.name}`, false))
-//       .catch(() => showPopUp(`Was unable to add ${dish.name}`, true))
-//   }
+  //   public static async createOrder(order: Order) {
+  //     const token = computed(() => store.state.apiToken)
+  //     await axios.post(`${this.API_URL}/dish/`, dish, {
+  //       headers: { Authorization: `Bearer ${token.value}`}
+  //     })
+  //       .then(() => showPopUp(`Added ${dish.name}`, false))
+  //       .catch(() => showPopUp(`Was unable to add ${dish.name}`, true))
+  //   }
 
-//   public static async approveOrder(dish: Dish) {
-//     const token = computed(() => store.state.apiToken)
-//     await axios.put(`${this.API_URL}/dish/${dish.id}`, dish, {
-//       headers: { Authorization: `Bearer ${token.value}`}
-//     })
-//       .then(() => showPopUp(`Updated ${dish.name}`, false))
-//       .catch(() => showPopUp(`Was unable to update ${dish.name}`, true))
-//   }
+  //   public static async approveOrder(dish: Dish) {
+  //     const token = computed(() => store.state.apiToken)
+  //     await axios.put(`${this.API_URL}/dish/${dish.id}`, dish, {
+  //       headers: { Authorization: `Bearer ${token.value}`}
+  //     })
+  //       .then(() => showPopUp(`Updated ${dish.name}`, false))
+  //       .catch(() => showPopUp(`Was unable to update ${dish.name}`, true))
+  //   }
 
-//   public static async deleteOrderById(dish: Dish) {
-//     const token = computed(() => store.state.apiToken)
-//     await axios.delete(`${this.API_URL}/dish/${dish.id}`, {
-//       headers: { Authorization: `Bearer ${token.value}`}
-//     })
-//       .then(() => showPopUp(`Deleted ${dish.name}`, false))
-//       .catch(() => showPopUp(`Was unable to delete ${dish.name}`, true))
-//   }
+  //   public static async deleteOrderById(dish: Dish) {
+  //     const token = computed(() => store.state.apiToken)
+  //     await axios.delete(`${this.API_URL}/dish/${dish.id}`, {
+  //       headers: { Authorization: `Bearer ${token.value}`}
+  //     })
+  //       .then(() => showPopUp(`Deleted ${dish.name}`, false))
+  //       .catch(() => showPopUp(`Was unable to delete ${dish.name}`, true))
+  //   }
 
-//   public static async removeOrdersBySessionId(dish: Dish) {
-//     const token = computed(() => store.state.apiToken)
-//     await axios.delete(`${this.API_URL}/dish/${dish.id}`, {
-//       headers: { Authorization: `Bearer ${token.value}`}
-//     })
-//       .then(() => showPopUp(`Deleted ${dish.name}`, false))
-//       .catch(() => showPopUp(`Was unable to delete ${dish.name}`, true))
-//   }
+  //   public static async removeOrdersBySessionId(dish: Dish) {
+  //     const token = computed(() => store.state.apiToken)
+  //     await axios.delete(`${this.API_URL}/dish/${dish.id}`, {
+  //       headers: { Authorization: `Bearer ${token.value}`}
+  //     })
+  //       .then(() => showPopUp(`Deleted ${dish.name}`, false))
+  //       .catch(() => showPopUp(`Was unable to delete ${dish.name}`, true))
+  //   }
 }
