d00dcccc55c7dcae3a8f8266e32f9b77c66b59ea
SiemLuc <48807736+SiemLuc@users.noreply.github.com>
2021-11-26 14:44:27 +0100

    Updated Ordercard order status

--- src/components/OrderCard.vue
+++ src/components/OrderCard.vue
@@ -22,7 +22,17 @@
                     <FlagIcon class="h-6 w-6" />
                 </div>
             </div>
-            <div v-else-if="!order.isApproved" class="card-header bg-danger relative">
+            <div v-else-if="!order.isApproved" class="card-header bg-primary relative">
+                <p class="text-xl">00:00</p>
+                <h5 class="text-xl">#{{order.id}}</h5>
+                <div class="absolute inset-y-3 right-5 flex space-x-2">
+                    <ReplyIcon class="w-6 h-6" />
+                    <BanIcon class="w-6 h-6" />
+                    <CheckIcon class="h-6 w-6" />
+                    <FlagIcon class="h-6 w-6" />
+                </div>
+            </div>
+            <div v-else class="card-header bg-danger relative">
                 <p class="text-xl">00:00</p>
                 <h5 class="text-xl">#{{order.id}}</h5>
                 <div class="absolute inset-y-3 right-5 flex space-x-2">
@@ -35,12 +45,9 @@
 
             <div class="card-body border bg-light text-black overflow-y-auto max-h-64">
               <p class="text-m text-gray-700 my-2" >{{order.comments}}</p>
-              <table class="table">
-                <tr>
-                    <td v-for="(dish, index) in order.dishes" :key="index">{{dish}}</td>
-                </tr>
-              </table>
-          </div>
+              <hr>
+              <div v-for="(dish, index) in order.dishes" :key="index">{{dish}}</div>  
+            </div>
     </div>
 </template>
 
