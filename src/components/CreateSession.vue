15ca4a438e395fe5ac9ffed03c1d8ef53049cbff
OwendB1 <52082575+OwendB1@users.noreply.github.com>
2021-11-26 10:18:42 +0100

    Update CreateSession.vue

--- src/components/CreateSession.vue
+++ src/components/CreateSession.vue
@@ -14,10 +14,10 @@ import stringGen from 'crypto-random-string'
 export default {
   setup() {
     const tableQuery = <string>router.currentRoute.value.query.tableid
-    if (tableQuery == null) return console.log('test')
+    if (tableQuery == null) return router.push('home')
     const tableId = Number.parseInt(tableQuery)
     console.log(`tableId: ${tableId}`)
-    let cookie = VueCookieNext.getCookie('GenericRestaurantSesh')
+    const cookie = VueCookieNext.getCookie('GenericRestaurantSesh')
     try { SessionDataService.getSessionByCookie(cookie) } catch { VueCookieNext.removeCookie('GenericRestaurantSesh')}
     let secret = stringGen({length: 25})
     const tableObj = RestaurantDataService.getTable(tableId)
@@ -31,8 +31,8 @@ export default {
           console.log(response)
           if (response == true) {
             RestaurantDataService.setInUse(tableId, true).then(val => {
-              console.log(val)
-              if(val) {                
+              console.log(val != null)
+              if(val != null) {
                 VueCookieNext.setCookie('GenericRestaurantSesh', `${secret}`)
                 router.push('session_page')
               } else {
