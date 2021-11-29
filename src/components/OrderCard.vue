5f2f56b2446f14197625141fa3099af7f5cc58ac
SiemLuc <48807736+SiemLuc@users.noreply.github.com>
2021-11-29 16:17:29 +0100

    Fixed ordering OrderCards on status

--- src/components/OrderCard.vue
+++ src/components/OrderCard.vue
@@ -1,8 +1,8 @@
 <template>
     <div  class="w-64 text-white mb-3" style="max-width: 18rem;">
         <!-- TODO: bg-color depends on order status -->
-            <!-- <div :load="log(orders)"></div> -->
-            <div v-if="order.isServed" class="card-header bg-success relative">
+            <!-- New order -->
+            <div v-if="order.isApproved && !order.isBeingPrepared && !order.isReady" class="card-header bg-primary relative">
                 <p class="text-xl">00:00</p>
                 <h5 class="text-xl">#{{order.id}}</h5>
                 <div class="absolute inset-y-3 right-5 flex space-x-2">
@@ -12,7 +12,8 @@
                     <FlagIcon class="h-6 w-6" />
                 </div>
             </div>
-            <div v-else-if="order.isApproved" class="card-header bg-warning relative">
+            <!-- Preparing order -->
+            <div v-else-if="order.isBeingPrepared" class="card-header bg-warning relative">
                 <p class="text-xl">00:00</p>
                 <h5 class="text-xl">#{{order.id}}</h5>
                 <div class="absolute inset-y-3 right-5 flex space-x-2">
@@ -22,7 +23,8 @@
                     <FlagIcon class="h-6 w-6" />
                 </div>
             </div>
-            <div v-else-if="!order.isApproved" class="card-header bg-danger relative">
+            <!-- Completed order -->
+            <div v-else-if="order.isReady" class="card-header bg-success relative">
                 <p class="text-xl">00:00</p>
                 <h5 class="text-xl">#{{order.id}}</h5>
                 <div class="absolute inset-y-3 right-5 flex space-x-2">
@@ -31,16 +33,32 @@
                     <CheckIcon class="h-6 w-6" />
                     <FlagIcon class="h-6 w-6" />
                 </div>
+            </div>
+            <!-- Canceled order -->
+            <div v-else-if="order.isCanceled" class="card-header bg-danger relative">
+                <p class="text-xl">00:00</p>
+                <h5 class="text-xl">#{{order.id}}</h5>
+                <div class="absolute inset-y-3 right-5 flex space-x-2">
+                    <ReplyIcon class="w-6 h-6" />
+                    <BanIcon class="w-6 h-6" />
+                    <CheckIcon class="h-6 w-6" />
+                    <FlagIcon class="h-6 w-6" />
+                </div>
+            </div>
+            <!-- Non approved order -->
+
+            <div v-else>
+
             </div>
 
-            <div class="card-body border bg-light text-black overflow-y-auto max-h-64">
+            <div v-if="order.isApproved || order.isCanceled" class="card-body border bg-light text-black overflow-y-auto max-h-64">
               <p class="text-m text-gray-700 my-2" >{{order.comments}}</p>
               <table class="table">
                 <tr>
                     <td v-for="(dish, index) in order.dishes" :key="index">{{dish}}</td>
                 </tr>
               </table>
-          </div>
+            </div>
     </div>
 </template>
 
--- src/views/LiveView.vue
+++ src/views/LiveView.vue
@@ -33,7 +33,7 @@ import OrderCard from '../components/OrderCard.vue'
 import store from '@/store'
 import { AuthPlugin } from '@/auth'
 import { Client } from '@stomp/stompjs'
-
+import { ref } from 'vue'
 export default defineComponent({
   components: {
     OrderCard
@@ -41,7 +41,78 @@ export default defineComponent({
   setup() {
     var client: Client
     const auth = inject<AuthPlugin>('Auth')
-    const orders = computed(() => store.state.orders)
+    // const orders = computed(() => store.state.orders)
+    const orders = [
+      { // Not approved order
+        id: 40,
+        sessionId: 1,
+        dishes: [
+          'steak',
+          'hamburgers'
+        ],
+        comments: '',
+        isApproved: false,
+        isCanceled: false,
+        isBeingPrepared: false,
+        isReady: false,
+        timeStamp: 10
+      },
+      { // New order (approved order)
+        id: 41,
+        sessionId: 1,
+        dishes: [
+          'steak',
+          'hamburgers'
+        ],
+        comments: '',
+        isApproved: true,
+        isCanceled: false,
+        isBeingPrepared: false,
+        isReady: false,
+        timeStamp: 10
+      },
+      { // Preparing order
+        id: 42,
+        sessionId: 1,
+        dishes: [
+          'steak',
+          'hamburgers'
+        ],
+        comments: 'hamburger without tomato',
+        isApproved: true,
+        isCanceled: false,
+        isBeingPrepared: true,
+        isReady: false,
+        timeStamp: 10
+      },
+      { // Completed order (done)
+        id: 43,
+        sessionId: 1,
+        dishes: [
+          'milkshake'
+        ],
+        comments: '',
+        isApproved: true,
+        isCanceled: false,
+        isBeingPrepared: false,
+        isReady: true,
+        timeStamp: 10
+      },
+      { // Cancelled (isCanceled)
+        id: 44,
+        sessionId: 1,
+        dishes: [
+          'steak'
+        ],
+        comments: '',
+        isApproved: false,
+        isCanceled: true,
+        isBeingPrepared: false,
+        isReady: false,
+        timeStamp: 10
+      }
+    ]
+
 
     onMounted(() => {
       // store.commit('setOrders')
