6549209f5e4caa944b554fe40892da7e8d72cb59
Daniel Vaswani <vaswanidaniel@gmail.com>
2021-11-30 14:51:05 +0100

    add real total to component
    Co-Authored-By: Ekhorn <44117179+Ekhorn@users.noreply.github.com>

--- src/components/CustomerOrderDialog.vue
+++ src/components/CustomerOrderDialog.vue
@@ -4,7 +4,7 @@
       class="fixed flex flex-col rounded-3xl p-3 m-4 h-20 mt-6 z-50 bottom-0 bg-white" 
       style="transform: translate(0.1vw); width: 92%; box-shadow: 0px 1px 2px 2px rgba(0, 0, 0, 0.1);">
       <div class="flex flex-row justify-between h-7">
-          <p class="text-2xl" style="color:#148F00">€{{40.34}}</p>
+          <p class="text-2xl" style="color:#148F00">€{{(total < 0 ? 0.00 : total).toFixed(2)}}</p>
           <p>Preparing...</p>
           <button class="rounded-lg px-1 text-sm w-16" style="color:#148F00; border: 2px solid;" @click=""> ORDER </button>
       </div>
@@ -23,13 +23,14 @@ export default defineComponent({
   setup () {
 
     const dishes = computed(() => store.state.currentOrder.dishes)
+    const total = computed(() => store.state.totalPrice)
 
     const countOccurrences = () => {
 
     }
     // dishes.value.reduce((a, v) => (v === props.dish.name ? a + 1 : a), 0)
 
-    return { message: ref('') }
+    return { message: ref(''), total}
   }
 })
 </script>
