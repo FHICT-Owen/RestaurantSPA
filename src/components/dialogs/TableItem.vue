5c9be224f10a73c912251e7ebce26f9f8e42ec73
Daniel Vaswani <vaswanidaniel@gmail.com>
2021-12-08 13:55:30 +0100

    Update TableItem.vue
    Co-Authored-By: Ekhorn <44117179+Ekhorn@users.noreply.github.com>

--- src/components/dialogs/TableItem.vue
+++ src/components/dialogs/TableItem.vue
@@ -1,30 +1,50 @@
 <template>
   <!-- <Table text="3" isEmpty=true active=false/> -->
-  <div class="table-row flex flex-row">
-    <div class="col-1">
+  <div
+    class="
+      flex flex-row
+      justify-between
+      items-center
+      max-w-3xl
+      whitespace-nowrap
+      rounded-2xl
+      px-4
+      py-2
+      m-1
+      shadow
+    "
+    style=""
+  >
+    <div class="flex items-center gap-2 w-full">
       <p class="num">Table {{ table.tableNumber }}</p>
       <div v-if="table.inUse" class="status emptyEqualsFalse">In Use</div>
       <div v-else class="status emptyEqualsTrue">Available</div>
     </div>
-    <div class="col-2">
-      <p v-if="table.isActive">Active</p>
-      <p v-else>Inactive</p>
-      <button class="toggle">
+    <div class="flex items-center gap-2 w-full">
+      <label class=" switch">
+        <input id="slider" type="checkbox" />
+        <span class="slider round "></span>
+      </label>
+      <!-- <button class="w-12">
         <img v-if="table.isActive" src="../../assets/toggle-on.png" />
         <img v-else src="../../assets/toggle-off.png" />
-      </button>
+      </button> -->
+     <label for="toggle" class=" text-gray-700">{{
+        table.isActive ? "Active" : "Inactive"
+      }}</label> 
     </div>
-    <button @click="addToSelectedTables" class="checkbox">
-      <img v-if="isSelected" src="../../assets/checkbox-checked.png" />
-      <img v-else src="../../assets/checkbox-unchecked.png" />
-    </button>
+    <label @click="true">
+      <input type="checkbox" />
+      <!-- <span class="slider round"></span> -->
+    </label>
   </div>
+  
 </template>
 
 
 <script lang="ts">
-import store from '@/store'
-import { computed, defineComponent, onMounted, PropType, ref } from 'vue'
+import store from "@/store";
+import { computed, defineComponent, onMounted, PropType, ref } from "vue";
 
 export default defineComponent({
   props: {
@@ -39,52 +59,93 @@ export default defineComponent({
   },
   setup(props) {
     function addToSelectedTables() {
-      store.commit('addToSelectedTableIds', props.table.id)
+      store.commit("addToSelectedTableIds", props.table.id);
     }
     function removeFromSelectedTables() {
       //store.commit("removeFromSelectedTableIds", props.table.id);
     }
-    function toggleTable(id: number) {
-
-    }
+    function toggleTable(id: number) {}
 
     //../assets/checkbox-checked.png
     //../assets/checkbox-unchecked.png
     // const checkboxImg: string = "unchecked"
 
-    onMounted(() => console.log(props.table.tableNumber))
+    onMounted(() => {
+      console.log(props.table.tableNumber) }
+    );
     return {
       addToSelectedTables,
       removeFromSelectedTables,
-      toggleTable
-    }
+      toggleTable,
+    };
   },
-})
+});
 </script>
 
 <style scoped>
-.table-row {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
-  max-width: 730px;
-  white-space: nowrap;
-  min-height: 55px;
-  padding-left: 10px;
-  padding-right: 10px;
-  box-shadow: -1px 1px 4px 1px rgba(0, 0, 0, 0.1);
-  border-radius: 25px;
-  margin-bottom: 10px;
+ /* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
 }
 
-p {
-  padding: 0;
-  margin: 0;
-  font-weight: normal;
-  font-size: 1rem;
-  line-height: 29px;
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
 }
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+} 
+
 .col-1,
 .col-2 {
   display: flex;
@@ -105,12 +166,4 @@ p {
   color: #009142;
 }
 
-.checkbox {
-  max-width: 35px;
-  max-height: 35px;
-}
-
-.toggle {
-  min-width: 50px;
-}
 </style>
