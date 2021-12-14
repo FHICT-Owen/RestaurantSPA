895dcafdd8aff4270b75f932af3d550648a7c062
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2021-12-14 10:58:06 +0100

    Added auto grow to order dialog textarea

--- src/components/dialogs/CustomerOrderDialog.vue
+++ src/components/dialogs/CustomerOrderDialog.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div 
-      class="fixed flex flex-col rounded-3xl p-3 m-4 h-20 mt-6 z-50 bottom-0 bg-white" 
+      class="fixed flex flex-col rounded-3xl p-3 m-4 mt-6 z-50 bottom-0 bg-white" 
       style="transform: translate(0.1vw); width: 92%; box-shadow: 0px 1px 2px 2px rgba(0, 0, 0, 0.1);">
       <div class="flex flex-row justify-between h-7">
           <p class="text-2xl" style="color:#148F00">€{{(total < 0 ? 0.00 : total).toFixed(2)}}</p>
@@ -9,7 +9,7 @@
           <button class="rounded-lg px-1 text-sm w-16" style="color:#148F00; border: 2px solid;" @click="order"> ORDER </button>
       </div>
       <div class="flex-row">
-        <textarea class="resize-none mt-1" v-model="comments" rows="1" placeholder="Comments..." />
+        <textarea class="resize-none mt-1" style="height: 24px" v-model="comments" rows="1" placeholder="Comments..." />
       </div>
     </div>
   </div>
@@ -17,7 +17,7 @@
 
 <script lang="ts">
 import store from '@/store'
-import { computed, defineComponent, ref } from 'vue'
+import { computed, defineComponent, onMounted, ref } from 'vue'
 
 export default defineComponent({
   props: {
@@ -30,12 +30,25 @@ export default defineComponent({
     const total = computed(() => store.state.totalPrice)
     let comments = ref('')
 
+    const onInput = (e: any) => {
+      e.target.style.height = 'auto'
+      e.target.style.height = (e.target.scrollHeight) + 'px'
+    }
+
     const order = () => {
       store.state.currentOrder.comments = comments.value
       props.placeOrder(store.state.currentOrder)
     }
 
-    return { comments, total, order }
+    onMounted(() => {
+      const tx = document.getElementsByTagName('textarea')
+      for (let i = 0; i < tx.length; i++) {
+        tx[i].setAttribute('style', 'height:' + (tx[i].scrollHeight) + 'px;overflow-y:hidden;')
+        tx[i].addEventListener('input', onInput, false)
+      }
+    })
+
+    return { comments, total, order, onInput }
   }
 })
 </script>
