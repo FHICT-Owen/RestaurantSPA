9867d60449f96ef77d7796ac6475571e9f43d9d6
OwendB1 <52082575+OwendB1@users.noreply.github.com>
2022-01-17 14:00:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const path = require('path')
 app = express()
-app.use(express.static(path.join(__dirname, '/dist/')))
+app.use(express.static(__dirname + '/dist/'))
 
 app.get(/.*/, function( req, res) {
-  res.sendfile( path.join(__dirname, '/dist/index.html'))
+  res.sendfile(__dirname + '/dist/index.html')
 })
 
 const port = process.env.PORT || 8080
