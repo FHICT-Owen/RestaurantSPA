b524ad45aca284ede10c91876399fb11a142df8b
Ekhorn <44117179+Ekhorn@users.noreply.github.com>
2022-01-17 17:05:11 +0100

    Update CreateSession.vue

--- src/components/CreateSession.vue
+++ src/components/CreateSession.vue
@@ -3,7 +3,6 @@
 </template>
 
 <script lang="ts">
-
 import router from '../router/index'
 import SessionDataService from '../services/SessionDataService'
 import RestaurantDataService from '../services/RestaurantDataService'
@@ -12,37 +11,32 @@ import { VueCookieNext } from 'vue-cookie-next'
 import stringGen from 'crypto-random-string'
 import TableDataService from '@/services/TableDataService'
 import { defineComponent } from '@vue/runtime-core'
+import store from '@/store'
 
 export default defineComponent({
   setup() {
     const tableQuery = <string>router.currentRoute.value.query.tableId
-    if (tableQuery != null) {
+    if (!!tableQuery) {
       const tableId = Number.parseInt(tableQuery)
       console.log(`tableId: ${tableId}`)
       const cookie = VueCookieNext.getCookie('GenericRestaurantSesh')
       try { SessionDataService.getSessionByCookie(cookie) } catch { VueCookieNext.removeCookie('GenericRestaurantSesh')}
-      let secret = stringGen({length: 25})
+      let secret = ''
       let tableObj = TableDataService.getTable(tableId)
       tableObj.then( async table => {
         if (table.isActive && !table.inUse) {
-          if(!!cookie) {
-            secret = cookie
-          }
-          const session = new Session(0, tableId, secret)
+          if(!!cookie) secret = cookie
           try {
-            await SessionDataService.createSession(session).then(response => {
+            await SessionDataService.createSession(new Session('', tableId)).then(response => {
               console.log(response)
-              if (response == true) {
-                RestaurantDataService.setInUse(tableId, true).then(val => {
-                  console.log(val)
-                  if(val != null) {
-                    VueCookieNext.setCookie('GenericRestaurantSesh', `${secret}`)
-                    router.push('session_page')
-                  } else {
-                    SessionDataService.removeSession(tableId)
-                  }
-                })
-              }
+              RestaurantDataService.setInUse(tableId, true).then(val => {
+                console.log(val)
+                if(!!val) {
+                  VueCookieNext.setCookie('GenericRestaurantSesh', `${secret}`)
+                  store.commit('setSessionTable', tableId)
+                  router.push('session_page')
+                } else SessionDataService.removeSession(tableId)
+              })
             })
           } catch { return router.push('/') }
         }
